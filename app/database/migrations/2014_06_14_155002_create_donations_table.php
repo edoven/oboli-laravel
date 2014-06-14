6a6ba7da7d8a9541e05db0f9ba8abbe52fd21a78
godzy <edoardo.venturini@gmail.com>
2014-06-14 18:01:11 +0200

    migrations debugged

--- app/database/migrations/2014_06_14_155002_create_donations_table.php
+++ app/database/migrations/2014_06_14_155002_create_donations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDonationTable extends Migration {
+class CreateDonationsTable extends Migration {
 
 	/**
 	 * Run the migrations.
