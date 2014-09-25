957c94332c829ce553648d403d1c0369fe152d04
godzy <edoardo.venturini@gmail.com>
2014-09-25 18:59:40 +0200

    app/config/facebook.php removed from git

--- app/config/facebook.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/config/facebook.php
-//prova commento
-
-// Facebook app Config 
-return array(
-        'appId' => '765916286805888',
-        'secret' => 'be984437ffbcd382df780ac39eb978eb'
-    );
