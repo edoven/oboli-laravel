3dfb542c3ef7a13518d05ab92c702f2405e3f270
godzy <edoardo.venturini@gmail.com>
2014-10-03 17:07:13 +0200

    cleaning UserController

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,37 +4,21 @@ class UserController extends BaseController {
 	
 	public function showAll()
 	{
-		$users = User::all();
-		
-		if (Request::is('api/v1/*'))
-		{
-			return Response::json(array(
-				'status' => 'success',
-				'users' => $users->toArray()),
-				200
-			);	
-		}
-		return View::make('users')->with('users', $users); 
+		return View::make('users')->with('users', User::all()); 
 	}
 	
-	public function showProfile($id)
+	public function showProfile($user_id)
 	{
-		if (Auth::id() != $id) //TODO handle this with exceptions
+		if (Auth::id() != $user_id) //TODO handle this with exceptions
 			App::abort(403, 'Access denied');
 		
-		$user = User::findOrFail($id);	//this already manage the web vs rest response (look at errors.php)
-		$donations = Donation::where('user_id', '=', $id)->get();
-		
-		if (Request::is('api/v1/*'))
-		{
-			return Response::json(array(
-				'status' => 'success',
-				'user' => $user->toArray(),
-				'donations' => $donations->toArray()),
-				200
-			);	
-		}
-		return View::make('user')->with('user', $user)->with('donations', $donations); 
+		$user = User::findOrFail($user_id);	//findOrFail also manages the web vs rest response (look at errors.php)
+		$donations = Donation::where('user_id', $user_id)->get();
+		$redeems = Codes::where('user', $user_id)->get();
+		return View::make('user')
+						->with('user', $user)
+						->with('donations', $donations)
+						->with('redeems', $redeems); ; 
 	}
 	
 	
@@ -44,20 +28,28 @@ class UserController extends BaseController {
 		$ngo_id = Input::get('ngo_id');
 		$amount = Input::get('amount'); //CHECK >0
 		if ($amount<1)
-			return "error with amount";
+			App::abort(400, 'The donation amount cannot be smaller than 1');
 		$ngo = Ngo::findOrFail($ngo_id);	
+		$user = User::findOrFail($user_id);	
 		try {
-			DB::beginTransaction();
-			$user = User::findOrFail($user_id);	
+			DB::beginTransaction();		
 			$user_oboli_count = $user['oboli_count'];
 			if ($user_oboli_count<$amount)
 			{
 				DB::connection()->getPdo()->rollBack();
-				return "not enought money";
+				App::abort(400, 'The donation amount is greater than the user obolis count');
 			}
-			DB::table('users')->where('id', $user_id)->update(array('oboli_count' => ($user_oboli_count-$amount), 'donated_oboli_count' => ($user->donated_oboli_count+$amount)));
-			DB::table('ngos')->where('id', $ngo_id)->update(array('oboli_count' => ($ngo['oboli_count']+$amount)));
-			DB::table('donations')->insert(array('user_id' => $user_id, 'ngo_id' => $ngo_id, 'amount' => $amount));		
+			DB::table('users')
+				->where('id', $user_id)
+				->update(array('oboli_count' => ($user_oboli_count-$amount), 
+							   'donated_oboli_count' => (($user->donated_oboli_count)+$amount)));
+			DB::table('ngos')
+				->where('id', $ngo_id)
+				->update(array('oboli_count' => ($ngo['oboli_count']+$amount)));
+			DB::table('donations')
+				->insert(array('user_id' => $user_id, 
+							   'ngo_id' => $ngo_id, 
+							   'amount' => $amount));		
 			DB::commit();
 		} catch (PDOException $e) {
 			DB::rollBack();
