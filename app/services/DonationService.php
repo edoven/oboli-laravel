4b28cf4ecdd08475a92d548313a2c17a681ed28a
godzy <edoardo.venturini@gmail.com>
2014-10-31 16:58:41 +0100

    DonationService: input validation function created

--- app/services/DonationService.php
+++ app/services/DonationService.php
@@ -4,30 +4,35 @@
 class DonationService
 {
 
-	//This methoss add a new donation, it does not check if the donator and the authenticated user are the same
-	public static function makeDonation($user_id, $ngo_id,	$amount)
+	private static function validateInput($user_id, $ngo_id, $amount)
 	{
 		$user = User::find($user_id);
 		if ($user == null)
 				return Utils::returnError('unexisting_user', null);
+		$ngo = Ngo::find($ngo_id);	
+		if ($ngo == null)
+			return Utils::returnError('unexisting_ngo', null);
 		if ($user->activated == 0 && (FacebookProfile::where('user_id', $user_id)->first()==null) )
 			return Utils::returnError('user_not_activated', null);
-
 		if ($amount<1)
 			return Utils::returnError('donation_amount_error', null);		
+		$user_oboli_count = $user->oboli_count;
+		if ($user_oboli_count<$amount)
+			return Utils::returnError('The donation amount is greater than the user obolis count', null);	
+		return Utils::returnSuccess(null,null);
+	}
+
+	//This methoss add a new donation, it does not check if the donator and the authenticated user are the same
+	public static function makeDonation($user_id, $ngo_id, $amount)
+	{
+		$validation = DonationService::validateInput($user_id, $ngo_id, $amount);
+		if ($validation['status']=='error')
+			return Utils::returnError($validation['message'],null);
+		$user = User::find($user_id);
+		$ngo = Ngo::find($ngo_id);	
+		$user_oboli_count = $user->oboli_count;
 		try {
-			DB::beginTransaction();		
-			$ngo = Ngo::find($ngo_id);	
-			if ($ngo == null)
-				return Utils::returnError('unexisting_ngo', null);
-					
-			$user_oboli_count = $user->oboli_count;
-			
-			if ($user_oboli_count<$amount)
-			{
-				DB::connection()->getPdo()->rollBack();
-				return Utils::returnError('The donation amount is greater than the user obolis count', null);
-			}
+			DB::beginTransaction();											
 			DB::table('users')
 				->where('id', $user_id)
 				->update(array('oboli_count' => ($user_oboli_count-$amount), 
