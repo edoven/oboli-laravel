b98e99be8de2745e5b062420f60db68dbeda57b7
godzy <edoardo.venturini@gmail.com>
2014-11-04 17:01:59 +0100

    splitted DonationController methods into rest and web

--- app/controllers/DonationController.php
+++ app/controllers/DonationController.php
@@ -6,32 +6,30 @@ include_once(app_path().'/utils.php');
 class DonationController extends BaseController {
 	
 
-	
-	
-	public function makeDonation()
+	public function makeDonationWeb()
 	{		
-		if (Request::is("api/*"))
-			$user_id = Input::get('user_id');
-		else
-			$user_id = Auth::id();
+		$user_id = Auth::id();
 		$ngo_id = Input::get('ngo_id');
 		$amount = Input::get('amount');
 		$return_array = DonationService::makeDonation($user_id, $ngo_id, $amount);
 		if ($return_array['status']=='error')
-			if (Request::is("api/*"))
-				return Utils::create_json_response("error", 400, $return_array['message'],null,array('user_id'=>$user_id, 'ngo_id'=>$ngo_id, 'amount'=>$amount));
-			else 
-				return 'Error: '.$return_array['message'];
+			return 'Error: '.$return_array['message'];
 		if ($return_array['status']=='success')
-			if (Request::is("api/*"))
-				return Utils::create_json_response("success", 200, 'a donation of '.$amount.' obolis to ngo '.$ngo_id.' has been made',null,array('user_id'=>$user_id, 'ngo_id'=>$ngo_id, 'amount'=>$amount));
-			else
-				return Redirect::to('ngos/'.$ngo_id);
+			return Redirect::to('ngos/'.$ngo_id);
+		App::abort(500, 'internal server error');
+	}
 
-		if (Request::is("api/*"))
-					return Utils::create_json_response("error", 500, 'internal server error',null,array('user_id'=>$user_id, 'ngo_id'=>$ngo_id, 'amount'=>$amount));
-			else
-				App::abort(500, 'internal server error');
+	public function makeDonationRest()
+	{		
+		$user_id = Input::get('user_id');
+		$ngo_id = Input::get('ngo_id');
+		$amount = Input::get('amount');
+		$return_array = DonationService::makeDonation($user_id, $ngo_id, $amount);
+		if ($return_array['status']=='error')
+			return Utils::create_json_response("error", 400, $return_array['message'], null, array('user_id'=>$user_id, 'ngo_id'=>$ngo_id, 'amount'=>$amount));
+		if ($return_array['status']=='success')
+			return Utils::create_json_response("success", 200, 'a donation of '.$amount.' obolis to ngo '.$ngo_id.' has been made',null,array('user_id'=>$user_id, 'ngo_id'=>$ngo_id, 'amount'=>$amount));
+		return Utils::create_json_response("error", 500, 'internal server error', null, array('user_id'=>$user_id, 'ngo_id'=>$ngo_id, 'amount'=>$amount));
 	}
 	
 
