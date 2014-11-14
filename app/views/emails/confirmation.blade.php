f5f29fae85cdd45645614571bbcd0248e396a26a
godzy <edoardo.venturini@gmail.com>
2014-11-14 12:46:03 +0100

    account confrimation email changed

--- app/views/emails/confirmation.blade.php
+++ app/views/emails/confirmation.blade.php
@@ -1,20 +1,37 @@
 <html>
 	<head>
+		 <style type="text/css">
+		   .btn {
+			   	background-color:#039be5;
+			   	border: 1px solid #1e3650;
+			   	border-radius:4px;
+			   	color: #ffffff;
+			   	display:inline-block;
+			   	font-family:sans-serif;
+			   	font-size:13px;
+			   	font-weight:bold;
+			   	line-height:40px;
+			   	text-align:center;
+			   	text-decoration:none;
+			   	width:300px;
+			   	-webkit-text-size-adjust:none;mso-hide:all;
+		    }
+		   .btn:hover {
+		   		background-color:#03a9f4;
+		   }
+		</style>
 	</head>
 	<body>
-		Ciao {{ $name }}, 
-		<br />
-		<br />
-		benvenuto in Oboli!
-		<br />
-		<br />
-		<p>
-			Per confermare l'account <a href="{{ $link }}">CLICCA QUI </a>.
-		</p>
-		<br />
-		<br />
-		<p>
-			Grazie e buona giornata
-		</p>
+		<div>
+			Ciao {{ $name }},<br />
+			benvenuto in Oboli!<br />
+			<br />
+			<a class="btn" href="{{ $link }}">Clicca per confermare il tuo account</a><br />
+			<br />
+			O clicca su questo link: </br>
+			<a href="{{ $link }}">{{ $link }}</a>
+
+
+		</div>
 	</body>
 </html>
