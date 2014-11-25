706059dc8b0bb544a0b144dbff737a1a060b2ae6
godzy <edoardo.venturini@gmail.com>
2014-11-25 12:02:45 +0100

    removed wrong file errors

--- app/models/Donation.php
+++ app/models/Donation.php
@@ -1,13 +1,14 @@
 <?php
 
 
-class MailinglistEntry extends Eloquent  {
+class Donation extends Eloquent  {
 
 	/**
 	 * The database table used by the model.
 	 *
 	 * @var string
 	 */
-	protected $table = 'mailinglist_entries';
+	protected $table = 'donations';
+
 
 }
