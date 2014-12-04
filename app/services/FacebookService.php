0f12138062016e54b432329876e012f2503d7a6e
godzy <edoardo.venturini@gmail.com>
2014-12-04 13:26:18 +0100

    multiple facebook_profile entries for same user with different access_token removed

--- app/services/FacebookService.php
+++ app/services/FacebookService.php
@@ -122,10 +122,10 @@ class FacebookService {
 		$user = User::where('email', $facebook_user_info['email'])->first(); 
 		if ($user == null)
 			$user = User::createConfirmedUser($facebook_user_info['email'], $facebook_user_info['name']);
-		if (FacebookProfile::where("user_id"=>($user->id))->first() == null)
+		if (FacebookProfile::where("user_id", $user->id)->first() == null)
 			FacebookProfile::create(array("user_id"=>($user->id), "uid"=>$facebook_user_info['id'], "access_token"=>$access_token));
 		else
-			$affectedRows = User::where('user_id', , $user->id)->update(array('access_token'=>$access_token));
+			FacebookProfile::where('user_id', , $user->id)->update(array('access_token'=>$access_token));
 		return Utils::returnSuccess("facebook_profile_created", array('user' => $user));
 	}
 
