8414a2df9dc579d1ccbfacf0405fadc470198a2b
godzy <edoardo.venturini@gmail.com>
2014-12-22 11:36:20 +0100

    config by ENV added

--- app/config/database.php
+++ app/config/database.php
@@ -56,9 +56,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'oboli',
-			'username'  => 'root',
-			'password'  => 'qwerty',
+			'database'  => $_ENV['DB'],
+			'username'  => $_ENV['DB_USER'],
+			'password'  => $_ENV['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
