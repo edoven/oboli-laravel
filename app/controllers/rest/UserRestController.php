241eb8e196b207b48683bdb978eb1ec74a9770b3
godzy <edoardo.venturini@gmail.com>
2015-01-21 15:10:37 +0100

    bug removed

--- app/controllers/rest/UserRestController.php
+++ app/controllers/rest/UserRestController.php
@@ -6,46 +6,6 @@ include_once(app_path().'/utils.php');
 class UserRestController extends BaseController {
 	
 
-
-
-	// public function showProfile($id)
-	// {
-	// 	Log::info('UserController::showProfileRest('.$id.')');
-	// 	$auth_user_id = Input::get('user_id');
-	// 	if ($auth_user_id != $id)
-	// 		return Utils::create_json_response("error", 401, 'Access denied', 'You can only access info for the auth user', null);	
-	// 	$user = User::find($id);
-	// 	if ($user == null)
-	// 		return Utils::create_json_response("error", 500, 'Internal Server Error', '', null);	
-	// 	$donations = Donation::where('user_id', $id)->get();
-	// 	$redeems = Code::where('user', $id)->get();
-
-	// 	$brands2obolis = DB::table('codes')->where('user', '=', $id)
-	// 						            ->join('products', 'codes.product', '=', 'products.id')
-	// 						            ->join('brands', 'products.brand', '=', 'brands.id')
-	// 						            ->select('brands.id as brand_id','brands.name as brand_name', DB::raw('sum(codes.oboli) as oboli'))
-	// 						            ->groupBy('brands.name')
-	// 						            ->get();
-
-	// 	$enriched_brands2obolis = array();
-	// 	foreach ($brands2obolis as $item) 
-	// 	{
-	// 		$enriched_item = array('brand_id' => $item->brand_id, 
-	// 							   'brand_name' => $item->brand_name, 
-	// 							   'oboli' => $item->oboli,
-	// 							   'brand_image_url' => Config::get('local-config')['host'].'/img/mobile/brands/'.$item->brand_id.'.jpg');
-	// 		array_push($enriched_brands2obolis, $enriched_item);
-	// 	}
-		
-	// 	return Utils::create_json_response("success", 200, null, null, 
-	// 								array('user' => $user->toArray(),
-	// 									  'donations' => $donations->toArray(),
-	// 									  'redeems'=>$redeems->toArray(),
-	// 									  'brands2obolis'=>$enriched_brands2obolis));
-	// }
-
-
-
 	public function showProfile($id)
 	{
 		Log::info('UserController::showProfileRest('.$id.')');
@@ -62,7 +22,6 @@ class UserRestController extends BaseController {
 										  'helped_ngos'=>$helped_ngos,
 										  'brands2obolis'=>$brands2obolis));
 	}
-
 
 
 	public function addPhoto()
@@ -83,15 +42,16 @@ class UserRestController extends BaseController {
 			return Utils::create_json_response('error', 400, 'image side is too large, max size is 1MB', null, null);
 		$file = Input::file('photo');
 		$destinationPath = public_path().'/img/users/';
-		$filename = $auth_user_id.'_'.str_random(20).'.$extension;
+		$filename = $auth_user_id.'_'.str_random(20).'.'.$extension; // example 1_ffgedsedfregfdrrthsf.jpg
 		$upload_success = $file->move($destinationPath, $filename);	
-
-		$user = User::find($auth_user_id);
-		$user->profile_image = $filename;
-		$user->save();
-		
+	
 		if( $upload_success ) 
+		{
+			$user = User::find($auth_user_id);
+			$user->profile_image = $filename;
+			$user->save();
 			return Utils::create_json_response('success', 200, null, null, array('url' => Config::get('local-config')['host'].'/img/users/'.$filename ));
+		}	
 		else
 			return Utils::create_json_response('error', 400, 'upload error', null, null);
 	}
